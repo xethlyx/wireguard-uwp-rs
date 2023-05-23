d324b5785a896667aab185140da4e973c3c492c6
xethlyx <46338199+xethlyx@users.noreply.github.com>
2023-05-23 01:37:48 -0400

    Add automated builds

---
+++ build.ps1
@@ -0,0 +1,25 @@
+$ErrorActionPreference = "Stop"
+
+function Invoke-Call {
+    param (
+        [scriptblock]$ScriptBlock,
+        [string]$ErrorAction = $ErrorActionPreference
+    )
+    & @ScriptBlock
+    if (($lastexitcode -ne 0) -and $ErrorAction -eq "Stop") {
+        exit $lastexitcode
+    }
+}
+
+if (Test-Path ".\dist") {
+    Remove-Item ".\dist" -Force -Recurse
+}
+
+Copy-Item -Recurse .\target\release .\dist\
+Copy-Item -Recurse .\appx\* .\dist\
+Invoke-Call -ScriptBlock { MakeAppx pack /d dist /p dist\wireguard } -ErrorAction Stop
+Write-Host "Make sure the publisher fields in .\appx\AppxManifest.xml match the fields in your certificate. For more details see https://learn.microsoft.com/en-us/uwp/schemas/appxpackage/appxmanifestschema/element-identity"
+$KeyLocation = Read-Host "Certificate location (.pfx file)"
+$Password = Read-Host "Certificate password"
+Invoke-Call -ScriptBlock { SignTool sign /fd SHA256 /a /f $KeyLocation /p $Password dist\wireguard.appx } -ErrorAction Stop
+Write-Host Done. appx is located at .\dist\wireguard.appx. Use "Add-AppxPackage -AllowUnsigned .\dist\wireguard.appx" to install.
